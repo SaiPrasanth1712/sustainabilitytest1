3fe003294bfadf780616473c338e9062f9ffe69e
SaiPrasanth1712 <saiprasanth1712@gmail.com>
2022-02-05 23:56:40 -0500

    initial commit

--- charts.js
+++ charts.js
@@ -11,6 +11,9 @@ var ESGE20=localStorage.getItem("hItem");
 var ESGE21=localStorage.getItem("termItem");
 var ESGE22=localStorage.getItem("disItem");
 var ESGE23=localStorage.getItem("epItem");
+var ESGE25=localStorage.getItem("piItem");
+var ESGE24=Math.round((Number(ESGE11)+Number(ESGE12)+Number(ESGE13)+Number(ESGE14)+Number(ESGE15)+Number(ESGE16)+Number(ESGE17)+Number(ESGE18)+Number(ESGE19)+Number(ESGE20)+Number(ESGE21)+Number(ESGE22)+Number(ESGE23))/15);
+var ESGE26=Math.round(Number(ESGE24)+ Number(ESGE25));
 
 var chartData = [ESGE11,ESGE12,ESGE13,ESGE14,ESGE15,ESGE16,ESGE17,ESGE18,ESGE19,ESGE20,ESGE21,ESGE22,ESGE23];
 
@@ -23,20 +26,37 @@ const myChart = new Chart(ctx, {
             label: 'ESG Performance Scores',
             data: chartData,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(82, 35, 100, 0.4)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(6, 102, 35, 0.5)',
+                'rgba(39, 70, 132, 0.5)',
+                'rgba(39, 70, 132, 0.5)',
+                'rgba(39, 70, 132, 0.5)',
+                'rgba(39, 70, 132, 0.5)',
+                'rgba(39, 70, 132, 0.8)'
+
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(82, 35, 100, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(6, 102, 35, 1)',
+                'rgba(39, 70, 132, 1)',
+                'rgba(39, 70, 132, 1)',
+                'rgba(39, 70, 132, 1)',
+                'rgba(39, 70, 132, 1)',
+                'rgba(39, 70, 132, 1)'
+
+
             ],
             borderWidth: 1
         }]
@@ -44,7 +64,42 @@ const myChart = new Chart(ctx, {
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                max: 100,
+                min: 0
+               
+            }
+        }
+    }
+});
+
+
+
+
+var overallChartData = [ESGE24, ESGE25, ESGE26]
+const osd = document.getElementById('myOverallChart').getContext('2d');
+const myOverallChart = new Chart(osd, {
+    type: 'bar',
+    data: {
+        labels: ['Average Performance', '+ Positive Impacts Bonus', 'Overall Score'],
+        datasets: [{
+            label: 'Overall Scores',
+            data: overallChartData,
+            backgroundColor: [
+                'rgba(82, 35, 100, 0.5)',
+            ],
+            borderColor: [
+                'rgba(82, 35, 100, 0.5)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                max: 100,
+                min: 0
                
             }
         }
